bc1ff78c05c82bd99720157c4bf6cd1ca802aa54
Li Ju <li.ju.0122@student.uu.se>
2019-12-12 15:05:20 +0100

    'implementation_for_task_1'

--- as3py2/task1.py
+++ as3py2/task1.py
@@ -54,14 +54,81 @@ the computed flow from u to v is given by F[u][v].
 def sensitive(G, s, t, F):
     """
     Sig:  graph G(V,E), int, int, int[0..|V|-1, 0..|V|-1] ==> int, int
-    Pre:
-    Post:
+    Pre:    G: a directed graph, in which every edge has its capacity;
+            s: flow starts from node s; t: flow ends at node t;
+            F: the given max flow matrix, in which flows each edge holds are stored
+    Post:   return a sensitive edge by giving its starting node and ending node
     Ex:   sensitive(G,0,5,F) ==> (1, 3)
     """
+    ## Function to construct residual graph of given graph with its flow
+    def res_graph(Graph, Flow):
+        """
+        Sig:  graph G(V,E), int[0..|V|-1, 0..|V|-1] ==> graph eG(V, E)
+        Pre:    G: a directed graph, in which every edge has its capacity;
+                F: the given max flow matrix, in which flows each edge holds are stored
+        Post:   return the residual graph of given graph and flow
+        Ex:   res_graph(G,F) ==> rG
+        """
+        rG = Graph.copy()
+        ## update the copy of original graph edge by edge
+        for start_node in Flow:
+            for end_node in Flow[start_node]:
+                ## add the reversed edge with capacity of current flow
+                rG.add_edge(end_node, start_node, capacity = Flow[start_node][end_node])
+                ## update the original edge's capacity to original capacity minus flow
+                rG[start_node][end_node]['capacity'] -= Flow[start_node][end_node]
+        return rG
+
+    ## build a function to explore all reachable nodes from a given node,
+    ## reach_dict is the dictionary recording the reachable nodes,
+    ## in which reachable nodes are marked as 1 while unreachable nodes are noted as -1
+    def explore(G, node, reach_dict):
+        """
+        Sig:  graph G(V,E), int, dictionary ==> dictionary
+        Pre:    G: a directed graph which is the residual flow graph for a graph and its flow;
+                node: node we start to explore
+                reach_dict: a dictionary in which all nodes are discovered or not from node
+                are recorded
+        Post:   return the dictionary in which every node are reachable or not are recorded:
+                if reachable, it is noted as1 otherwise it's noted as -1
+        Ex:   explore(G,s,reach_dict) ==> reach_dict
+        """
+        reach_dict[node] = 1
+        ## a DFS-like approach, to explore each node directly linked with current node
+        for each in G[node]:
+            if G[node][each]['capacity'] > 0 and reach_dict[each] == -1:
+                explore(G, each, reach_dict)
+        return reach_dict
+
+    ## construct the residual graph rG of given graph G
+    rG = res_graph(G, F)
+    ## build a the status dictionary and mark every node in rG as -1,
+    ## which means unreachable (currently)
+    reachable_dict = dict.fromkeys(list(rG.nodes), -1)
+    ## explore the graph from the the node s, to find all reachable nodes
+    reachable_dict = explore(rG, s, reachable_dict)
+
+    ## find all saturated edges, whose capacity equals the flow it holds
+    satur_edges = []
     for start_node in F:
         for end_node in F[start_node]:
             if F[start_node][end_node] == G[start_node][end_node]["capacity"]:
-                return start_node, end_node
+                satur_edges.append([start_node, end_node])
+
+    ## NOTICE: not all saturated edges are sensitive edges -- only those which are in minimum
+    ## cut are sensitive
+    sens_edges = []
+    for each in satur_edges:
+        ## to find which saturated edges are among the minimum cut
+        if reachable_dict[each[0]] + reachable_dict[each[1]] == 0:
+            sens_edges.append(each)
+    ## return any edge of sensitive edges: because of the theorem of maxflow-minicut, it is
+    ## guaranteed that there is at least one edge is sensitive
+    return sens_edges[0]
+
+
+
+
 
 
 class SensitiveSanityCheck(unittest.TestCase):
@@ -139,15 +206,23 @@ class SensitiveSanityCheck(unittest.TestCase):
         """Sanity check"""
         # The attribute on each edge MUST be called "capacity"
         # (otherwise the max flow algorithm in run_test will fail).
-        self.G.add_edge(0, 1, capacity = 16)
-        self.G.add_edge(0, 2, capacity = 13)
-        self.G.add_edge(2, 1, capacity = 4)
-        self.G.add_edge(1, 3, capacity = 12)
-        self.G.add_edge(3, 2, capacity = 9)
-        self.G.add_edge(2, 4, capacity = 14)
-        self.G.add_edge(4, 3, capacity = 7)
-        self.G.add_edge(3, 5, capacity = 20)
-        self.G.add_edge(4, 5, capacity = 4)
+        # self.G.add_edge(0, 1, capacity = 16)
+        # self.G.add_edge(0, 2, capacity = 11)
+        # self.G.add_edge(2, 1, capacity = 4)
+        # self.G.add_edge(1, 3, capacity = 12)
+        # self.G.add_edge(3, 2, capacity = 9)
+        # self.G.add_edge(2, 4, capacity = 14)
+        # self.G.add_edge(4, 3, capacity = 7)
+        # self.G.add_edge(3, 5, capacity = 20)
+        # self.G.add_edge(4, 5, capacity = 30)
+        self.G.add_edge(0, 1, capacity = 2)
+        self.G.add_edge(1, 2, capacity=1)
+        self.G.add_edge(0, 3, capacity=1)
+        self.G.add_edge(3, 4, capacity=1)
+        self.G.add_edge(1, 4, capacity=1)
+        self.G.add_edge(3, 2, capacity=1)
+        self.G.add_edge(2, 5, capacity=1)
+        self.G.add_edge(4, 5, capacity=2)
         self.run_test(0,5,draw=True)
 
     @classmethod
