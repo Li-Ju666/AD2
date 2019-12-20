ea590e9755c5eca4dae8b23ef06b967b415cea05
Li Ju <li.ju.0122@student.uu.se>
2019-12-20 02:02:21 +0100

    report

---
+++ as3py2/for report/new/party_seating.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+'''
+Assignment 3, Problem 2: Party Seating
+
+Team Number:   55
+Student Names: Li Ju, Georgios Panayiotou
+'''
+
+'''
+Copyright: Pierre.Flener@it.uu.se and his teaching assistants, 2019.
+
+This file is part of course 1DL231 at Uppsala University, Sweden.
+
+Permission is hereby granted only to the registered students of that
+course to use this file, for a homework assignment.
+
+The copyright notice and permission notice above shall be included in
+all copies and extensions of this file, and those are not allowed to
+appear publicly on the internet, both during a course instance and
+forever after.
+'''
+
+import unittest
+
+# If your solution needs a queue, then you can use this one:
+from collections import deque
+
+def party(known):
+
+    ## This function used a DFS-like algorithm, exploring the graph from a random undiscovered node. As problem described,
+    ## not two connected nodes are supposed to arranged in one table, so along the edge we arrange. The starting node
+    ## and the ending node of a edge is arranged in two list. If following all edges every node is put into 2 lists perfectly
+    ## without any contradiction, the arrangement is available, otherwise a contradiction (two arranged nodes which are in
+    ## the same list are connected) will occur and a False is returned. As all nodes and edges are traversed, therefore,
+    ## the time complexity of the algorithm is like classic DFS, O(|E|+|V|).
+    """
+    Sig:  int[1..m, 1..n] ==> boolean, int[1..j], int[1..k]
+    Pre:  a list of list, in which stored the edges between nodes
+    Post: a boolean indicating if such arrangement can be made, if True, two list containing the plan are returned,
+          otherwise two empty lists are returned.
+    Ex:   [[1,2],[0],[0]] ==> True, [0], [1,2]
+    """
+    ## construct a dictionary to store the current status of every node, 0 means the node has not been arranged,
+    ## 1 means it is set in the list1, while -1 means it is set in the list2. The initial status is all nodes are 0.
+    status_dict = dict.fromkeys(range(len(known)), 0)
+
+    ## A dictionary for storage of unarranged nodes:
+    ## the reason for using dictionary instead of list is that the complexity of dictionary's deletion is of constant time,
+    ## removal of an arranged node requires operated |nodes| times, this is used for reduction of time complexity.
+    unarranged_dict = dict.fromkeys(range(len(known)), 0)
+
+    ## The default result if such arrangement is available is True
+    result = True
+
+    ## the function arrange is defined to arrange the given node and its children recursively
+    def arrange(node, parent):
+        ## update the status of current node in status_dictionary according to its parent's status: opposite to its
+        ## parent's status
+        if parent is None:
+            status_dict[node] = 1
+            linked_nodes = known[node]
+        else:
+            status_dict[node] = -status_dict[parent]
+            linked_nodes = known[node]
+            linked_nodes.remove(parent)
+
+        ## delete the current node from dictionary of unarranged nodes
+        unarranged_dict.pop(node)
+
+        ## The default result if such arrangement is available is True, if every node is put with no contradiction,
+        ## it keeps True
+        result = True
+
+        ## arrange given node's children
+        if linked_nodes is not None:
+            for each in linked_nodes:
+                ## if contradiction occurs, end the loop
+                if result == True:
+                    ## a child's status equals its own status: contradiction occurs
+                    if status_dict[each] == status_dict[node]:
+                        result = False
+                    ## the child has not been arranged: recursively arrange the child
+                    elif status_dict[each] == 0:
+                        # Recursive variant: number of not explored nodes in known decreases
+                        result = arrange(each, node)
+        return result
+
+    ## arrange all the unarranged nodes: if contradiction occurs, end the loop
+    # Loop variant: number of not arranged nodes in known decreases
+    while result is True and len(unarranged_dict) > 0:
+        # Recursive variant: number of not arranged nodes in known decreases
+        result = arrange(unarranged_dict.keys()[0], None)
+    table1, table2 = [], []
+    if result == True:
+        ## traverse the status dictionary: nodes whose value are 1 is put in list1, while those whose value is -1
+        ## are appended in list2
+        for each in status_dict:
+            if status_dict[each] == 1:
+                table1.append(each)
+            else: table2.append(each)
+    return result, table1, table2
+
+class PartySeatingTest(unittest.TestCase):
+    """Test suite for party seating problem
+    """
+
+    def test_sanity(self):
+        """Sanity test
+
+        A minimal test case.
+        """
+        K = [[1,2],[0],[0]]
+        (found, A, B) = party(K)
+        self.assertEqual(
+            len(A) + len(B),
+            len(K),
+            "wrong number of guests: {!s} guests, tables hold {!s} and {!s}".format(
+                len(K),
+                len(A),
+                len(B)
+                )
+            )
+        for g in range(len(K)):
+            self.assertTrue(
+                g in A or g in B,
+                "Guest {!s} not seated anywhere".format(g))
+        for a1 in A:
+            for a2 in A:
+                self.assertFalse(
+                    a2 in K[a1],
+                    "Guests {!s} and {!s} seated together, and know each other".format(
+                        a1,
+                        a2
+                        )
+                    )
+        for b1 in B:
+            for b2 in B:
+                self.assertFalse(
+                    b2 in K[b1],
+                    "Guests {!s} and {!s} seated together, and know each other".format(
+                        b1,
+                        b2
+                        )
+                    )
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ as3py2/for report/new/sensitive.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+'''
+Assignment 3, Problem 1: Controlling the Maximum Flow
+
+Team Number:   55
+Student Names: Li Ju, Georgios Panayiotou
+'''
+
+'''
+Copyright: Pierre.Flener@it.uu.se and his teaching assistants, 2019.
+
+This file is part of course 1DL231 at Uppsala University, Sweden.
+
+Permission is hereby granted only to the registered students of that
+course to use this file, for a homework assignment.
+
+The copyright notice and permission notice above shall be included in
+all copies and extensions of this file, and those are not allowed to
+appear publicly on the internet, both during a course instance and
+forever after.
+'''
+
+import unittest
+
+import networkx as nx
+# API for NetworkX flow algorithms changed in v1.9:
+if (list(map(lambda x: int(x), nx.__version__.split("."))) < [1, 9]):
+    max_flow = nx.ford_fulkerson
+else:
+    max_flow = nx.maximum_flow
+"""
+We use max_flow() to generate flows for the included tests, and you
+may of course use it as well in any tests you generate.  As always,
+your implementation of the senstive() function may NOT make use of
+max_flow(), or any of the other graph algorithm implementations
+provided by NetworkX.
+"""
+
+# If your solution needs a queue (like the BFS algorithm), you can use this one:
+from collections import deque
+
+try:
+    import matplotlib.pyplot as plt
+    HAVE_PLT = True
+except ImportError:
+    HAVE_PLT = False
+
+"""
+F is represented in python as a dictionary of dictionaries;
+i.e., given two nodes u and v,
+the computed flow from u to v is given by F[u][v].
+"""
+def sensitive(G, s, t, F):
+    """
+    Sig:  graph G(V,E), int, int, int[0..|V|-1, 0..|V|-1] ==> int, int
+    Pre:    G: a directed graph, in which every edge has its capacity;
+            s: flow starts from node s; t: flow ends at node t;
+            F: the given max flow matrix, in which flows each edge holds are stored
+    Post:   return a sensitive edge by giving its starting node and ending node
+    Ex:   sensitive(G,0,5,F) ==> (1, 3)
+    """
+    ## Function to construct residual graph of given graph with its flow
+    def res_graph(Graph, Flow):
+        """
+        Sig:  graph G(V,E), int[0..|V|-1, 0..|V|-1] ==> graph eG(V, E)
+        Pre:    G: a directed graph, in which every edge has its capacity;
+                F: the given max flow matrix, in which flows each edge holds are stored
+        Post:   return the residual graph of given graph and flow
+        Ex:   res_graph(G,F) ==> rG
+        """
+        rG = Graph.copy()
+        ## update the copy of original graph edge by edge
+        # Loop variant: number of nodes in G not added to rG decreases
+        for start_node in Flow:
+            # Loop variant: number of nodes in a flow starting from start_node not checked decreases
+            for end_node in Flow[start_node]:
+                ## add the reversed edge with capacity of current flow
+                rG.add_edge(end_node, start_node, capacity = Flow[start_node][end_node])
+                ## update the original edge's capacity to original capacity minus flow
+                rG[start_node][end_node]['capacity'] -= Flow[start_node][end_node]
+        return rG
+
+    ## build a function to explore all reachable nodes from a given node,
+    ## reach_dict is the dictionary recording the reachable nodes,
+    ## in which reachable nodes are marked as 1 while unreachable nodes are noted as -1
+    def explore(G, node, reach_dict):
+        """
+        Sig:  graph G(V,E), int, dictionary ==> dictionary
+        Pre:    G: a directed graph which is the residual flow graph for a graph and its flow;
+                node: node we start to explore
+                reach_dict: a dictionary in which all nodes are discovered or not from node
+                are recorded
+        Post:   return the dictionary in which every node are reachable or not are recorded:
+                if reachable, it is noted as1 otherwise it's noted as -1
+        Ex:   explore(G,s,reach_dict) ==> reach_dict
+        """
+        reach_dict[node] = 1
+        ## a DFS-like approach, to explore each node directly linked with current node
+        # Loop variant: number of nodes in graph G not checked decreases
+        for each in G[node]:
+            if G[node][each]['capacity'] > 0 and reach_dict[each] == -1:
+                # Recursive variant: number of unexplored nodes in G decreases
+                explore(G, each, reach_dict)
+        return reach_dict
+
+    ## construct the residual graph rG of given graph G
+    rG = res_graph(G, F)
+    ## build a the status dictionary and mark every node in rG as -1,
+    ## which means unreachable (currently)
+    reachable_dict = dict.fromkeys(list(rG.nodes), -1)
+    ## explore the graph from the the node s, to find all reachable nodes
+    # Recursive variant: number of nodes in residual graph unexplored decreases
+    reachable_dict = explore(rG, s, reachable_dict)
+
+    ## find all saturated edges, whose capacity equals the flow it holds
+    satur_edges = []
+    # Loop variant: number of nodes in flow F not checked decreases
+    for start_node in F:
+        # Loop variant: number of nodes in a flow starting from start_node not checked decreases
+        for end_node in F[start_node]:
+            if F[start_node][end_node] == G[start_node][end_node]["capacity"]:
+                satur_edges.append([start_node, end_node])
+
+    ## NOTICE: not all saturated edges are sensitive edges -- only those which are in minimum
+    ## cut are sensitive
+    sens_edges = []
+    # Loop variant: saturated edges not checked decreases
+    for each in satur_edges:
+        ## to find which saturated edges are among the minimum cut
+        if reachable_dict[each[0]] == -1 and reachable_dict[each[1]] == 1:
+            sens_edges.append(each)
+    ## return any edge of sensitive edges: because of the theorem of maxflow-minicut, it is
+    ## guaranteed that there is at least one edge is sensitive
+    return sens_edges[0]
+
+class SensitiveSanityCheck(unittest.TestCase):
+    """
+    Test suite for the sensitive edge problem
+    """
+    def draw_graph(self, H, u, v, flow1, F1, flow2, F2):
+        if not HAVE_PLT:
+            return
+        pos = nx.spring_layout(self.G)
+        plt.subplot(1,2,1)
+        plt.axis('off')
+        nx.draw_networkx_nodes(self.G,pos)
+        nx.draw_networkx_edges(self.G,pos)
+        nx.draw_networkx_labels(self.G,pos)
+        nx.draw_networkx_edge_labels(
+            self.G, pos,
+            edge_labels={(u,v):'{}/{}'.format(
+                  F1[u][v],
+                  self.G[u][v]['capacity']
+                ) for (u,v,data) in nx.to_edgelist(self.G)})
+        plt.title('before: flow={}'.format(flow1))
+        plt.subplot(1,2,2)
+        plt.axis('off')
+        nx.draw_networkx_nodes(self.G,pos)
+        nx.draw_networkx_edges(self.G,pos)
+        nx.draw_networkx_edges(
+            self.G, pos,
+            edgelist=[(u,v)],
+            width=3.0,
+            edge_color='b')
+        nx.draw_networkx_labels(self.G,pos)
+        nx.draw_networkx_edge_labels(
+            self.G, pos,
+            edge_labels={(u,v):'{}/{}'.format(
+                  F2[u][v],H[u][v]['capacity']
+                ) for (u,v,data) in nx.to_edgelist(self.G)})
+        plt.title('after: flow={}'.format(flow2))
+
+    def setUp(self):
+        """start every test with an empty directed graph"""
+        self.G = nx.DiGraph()
+
+    def run_test(self, s, t, draw=False):
+        """standard tests to run for all cases
+
+        Uses networkx to generate a maximal flow
+        """
+        H = self.G.copy()
+        # compute max flow
+        flow_g, F_g = max_flow(self.G, s, t)
+        # find a sensitive edge
+        u,v = sensitive(self.G, s, t, F_g)
+        # is u a node in G?
+        self.assertIn(u, self.G, "Invalid edge ({}, {})".format(u ,v))
+        # is (u,v) an edge in G?
+        self.assertIn(v, self.G[u], "Invalid edge ({}, {})".format(u ,v))
+        # decrease capacity of (u,v) by 1
+        H[u][v]["capacity"] = H[u][v]["capacity"] - 1
+        # recompute max flow
+        flow_h, F_h = max_flow(H, s, t)
+        if draw:
+            self.draw_graph(H, u, v, flow_g, F_g, flow_h, F_h)
+        # is the new max flow lower than the old max flow?
+        self.assertLess(
+            flow_h,
+            flow_g,
+            "Returned non-sensitive edge ({},{})".format(u,v))
+
+    def test_sanity(self):
+        """Sanity check"""
+        # The attribute on each edge MUST be called "capacity"
+        # (otherwise the max flow algorithm in run_test will fail).
+        self.G.add_edge(0, 1, capacity = 16)
+        self.G.add_edge(0, 2, capacity = 13)
+        self.G.add_edge(2, 1, capacity = 4)
+        self.G.add_edge(1, 3, capacity = 12)
+        self.G.add_edge(3, 2, capacity = 9)
+        self.G.add_edge(2, 4, capacity = 14)
+        self.G.add_edge(4, 3, capacity = 7)
+        self.G.add_edge(3, 5, capacity = 20)
+        self.G.add_edge(4, 5, capacity = 4)
+        self.run_test(0,5,draw=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        if HAVE_PLT:
+            plt.show()
+
+if __name__ == "__main__":
+    unittest.main()
